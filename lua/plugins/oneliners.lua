f39d7ff68ddeda0dba800f4d42d597e12001eb1a
ElijahInamarga <elijahcek@gmail.com>
2025-08-15 21:48:56 -0700

    New lualine theme and removed unnecessary CSS highlight plugin

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -2,10 +2,4 @@ return {
     { -- Git plugin
 	'tpope/vim-fugitive',
     },
-    { -- Show CSS Colors
-	'brenoprata10/nvim-highlight-colors',
-	config = function()
-	    require('nvim-highlight-colors').setup({})
-	end
-    },
 }
