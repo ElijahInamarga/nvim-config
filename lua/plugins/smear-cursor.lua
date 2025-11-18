6be55769f27dd43e34230ff654a52e84469bbe97
ElijahInamarga <elijahcek@gmail.com>
2025-11-18 10:37:52 -0800

    Disabled smear cursor

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,7 +1,7 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    enabled = true,
+    enabled = false,
     stiffness = 0.9,
     trailing_stifness = 0.8,
     distance_stop_animating = 0.3
