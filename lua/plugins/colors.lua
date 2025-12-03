1cf5b86d0333d210078d266128b851e7acdb4ec3
ElijahInamarga <elijahcek@gmail.com>
2025-12-02 22:21:52 -0800

    New font and theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,14 @@
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  name = "kanagawa",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme kanagawa-dragon")
-  end
+  {
+    "metalelf0/black-metal-theme-neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- This setup function selects and loads the theme called 'gorgoroth'
+      require("black-metal").setup({
+        theme = "gorgoroth",
+      })
+      require("black-metal").load() -- Apply the theme
+    end,
+  },
 }
