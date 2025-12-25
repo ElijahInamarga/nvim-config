024e795402dd0cac6d846add4df53d1b240851ad
ElijahInamarga <elijahcek@gmail.com>
2025-12-25 14:33:02 -0800

    Added custom cursor line and status line

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,5 @@
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.g.mapleader = " "
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -9,6 +9,13 @@ return {
         theme = "gorgoroth",
       })
       require("black-metal").load() -- Apply the theme
+
+      -- Custom cursor line and status line 
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222", underline = false })
+      vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e15d44", bold = true })
+
+      vim.api.nvim_set_hl(0, "StatusLine", { bg = "#1a1a1a", fg = "#bcbcbc"})
+      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#121212", fg = "#585858" })
     end,
   },
 }
