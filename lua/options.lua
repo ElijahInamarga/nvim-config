69b896a993ea5c06f043288193ff97ad1a5a1dc3
ElijahInamarga <elijahcek@gmail.com>
2026-01-30 19:33:21 -0800

    automatically switch tab with 8 spaces

--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,12 @@ vim.opt.shiftwidth = 8
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp", "h" },
+  callback = function()
+    vim.opt_local.tabstop = 8      -- Render \t as 8 spaces
+    vim.opt_local.shiftwidth = 8   -- Size of an indent
+    vim.opt_local.expandtab = true -- Convert tabs to spaces
+  end,
+})
