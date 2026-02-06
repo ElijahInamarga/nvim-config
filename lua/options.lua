8967142c2782477ce6e6bc271af873e66bb3dd78
ElijahInamarga <elijahcek@gmail.com>
2026-02-05 21:58:47 -0800

    Removed auto /t -> space

--- lua/options.lua
+++ lua/options.lua
@@ -1,14 +1,5 @@
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 8
+vim.opt.shiftwidth = 4
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp", "h" },
-  callback = function()
-    vim.opt_local.tabstop = 8      -- Render \t as 8 spaces
-    vim.opt_local.shiftwidth = 8   -- Size of an indent
-    vim.opt_local.expandtab = true -- Convert tabs to spaces
-  end,
-})
